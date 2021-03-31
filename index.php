ca3c33ed42bf3f73e3aaa8c812ead9c4a062171f
taoka-toshiaki <71550806+taoka-toshiaki@users.noreply.github.com>
2021-03-31 16:34:11 +0900

    Update index.php
    短縮化

--- index.php
+++ index.php
@@ -5,39 +5,18 @@ class moon
     public const GREGORIAN = array(0, 2, 0, 2, 2, 4, 5, 6, 7, 8, 9, 10);
     public function main($year = 2012, $mon = 12, $day = 12)
     {
-        $ICON = (object) [
-            (object) ["moon" => '🌑'], //朔
-            (object) ["moon" => '🌑'], //朔
-            (object) ["moon" => '🌑'], //朔
-            (object) ["moon" => '🌒'], //三日月
-            (object) ["moon" => '🌒'], //三日月
-            (object) ["moon" => '🌒'], //三日月
-            (object) ["moon" => '🌒'], //三日月
-            (object) ["moon" => '🌓'], //上弦の月',
-            (object) ["moon" => '🌓'], //上弦の月',
-            (object) ["moon" => '🌓'], //上弦の月',
-            (object) ["moon" => '🌓'], //上弦の月',
-            (object) ["moon" => '🌓'], //上弦の月',
-            (object) ["moon" => '🌔'], //十三夜月
-            (object) ["moon" => '🌔'], //十三夜月
-            (object) ["moon" => '🌔'], //十三夜月
-            (object) ["moon" => "🌕"], //望月
-            (object) ["moon" => "🌕"], //望月
-            (object) ["moon" => "🌕"], //望月
-            (object) ["moon" => "🌕"], //望月
-            (object) ["moon" => "🌖"], //寝待月
-            (object) ["moon" => "🌖"], //寝待月
-            (object) ["moon" => "🌖"], //寝待月
-            (object) ["moon" => "🌖"], //寝待月
-            (object) ["moon" => '🌗'], //下弦の月
-            (object) ["moon" => '🌗'], //下弦の月
-            (object) ["moon" => '🌗'], //下弦の月
-            (object) ["moon" => "🌘"], //二十六夜月
-            (object) ["moon" => "🌘"], //二十六夜月
-            (object) ["moon" => "🌘"], //二十六夜月
-            (object) ["moon" => "🌘"] //二十六夜月
-        ];      
-        return $ICON->{(int)(((((int)$year - 11) % 19) * 11 + static::GREGORIAN[(int)$mon - 1] + (int)$day) % 30)}->moon;
+        $cnt=0;
+        $start = base_convert(bin2hex('🌑'), 16, 10);//U+1F311
+        $moon_age =[3,4,5,3,4,4,3,4];
+        $moon_age_sum = $moon_age[0];
+        for($i=0;$i<30;$i++){
+            if($i>($moon_age_sum - 1)){
+                $cnt++;
+                $moon_age_sum = $moon_age_sum + $moon_age[$cnt];
+            }
+            $moon_emoji[] = hex2bin(base_convert(($start + $cnt), 10, 16));
+        }
+        return $moon_emoji[(int)(((((int)$year - 11) % 19) * 11 + static::GREGORIAN[(int)$mon - 1] + (int)$day) % 30)];
     }
 }
 
